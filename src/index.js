859d0f4a80fd062b1e2b0565a53a3c5d76714957
Alexey Pargachev <pargachev20@gmail.com>
2022-03-01 11:44:07 +0700

    add classes

--- src/index.js
+++ src/index.js
@@ -16,310 +16,186 @@ loader.instantiate(fetch('./build/optimized.wasm')).then(({ exports }) => {
   const { __newArray, __getArray, __pin, __unpin } = exports;
 
   // Константы и настройки
-  let isMouseDown = false;
-  let weights = []; // массив весов
-  let pixels = []; // массив вектора (0 - пусто, 1 - закрашено)
-  let canvas = document.createElement('canvas');
-  let ctx = canvas.getContext('2d');
-  let currentColor = '#000000'; // цвет линии
-  const currentBg = 'white'; // цвет фона
-  const currentSize = 2; // толщина линии
-  const canvasSize = 150; // поле
   let neuronSum = 0; // сумма нейрона
   let sigmoidRes = 0; // ответ сигмоиды
+
+  class Canvas {
+    canvas = document.createElement('canvas');
+    ctx = this.canvas.getContext('2d');
+    #currentColor = '#000000'; // цвет линии
+    #currentBg = 'white'; // цвет фона
+    #currentSize = 2; // толщина линии
+    canvasSize = 50; // размер поля
+    #isMouseDown = false;
+    vectors = []; // массив векторов (0 - пусто, 1 - закрашено)
+
+    constructor() {
+      this.createCanvas();
+    }
+
+    // Создание поля
+    createCanvas() {
+      this.canvas.id = 'canvas';
+      this.canvas.width = parseInt(this.canvasSize);
+      this.canvas.height = parseInt(this.canvasSize);
+      this.canvas.style.zIndex = 8;
+      this.canvas.style.border = '1px solid';
+      this.ctx.fillStyle = this.#currentBg;
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+      document.getElementById('main').appendChild(this.canvas);
+    }
+
+    // Очистка поля
+    clear() {
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    // Получение векторов
+    getVectors() {
+      const data = this.ctx.getImageData(
+        0,
+        0,
+        this.canvasSize,
+        this.canvasSize
+      );
+      const arr = Array.from(data.data);
+
+      const pixelArr = __pin(__newArray(Int32Array_ID, arr));
+
+      const vectorArr = __getArray(__pin(toImage(pixelArr, this.canvasSize)));
+
+      this.vectors = vectorArr;
+
+      __unpin(pixelArr);
+      __unpin(vectorArr);
+    }
+
+    // Позиция мыши
+    #getMousePos(evt) {
+      let rect = this.canvas.getBoundingClientRect();
+      return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top,
+      };
+    }
+
+    // Кнопка мыши зажата
+    mousedown(evt) {
+      this.#isMouseDown = true;
+      let currentPosition = this.#getMousePos(evt);
+      this.ctx.moveTo(currentPosition.x, currentPosition.y);
+      this.ctx.beginPath();
+      this.ctx.lineWidth = this.#currentSize;
+      this.ctx.lineCap = 'round';
+      this.ctx.strokeStyle = this.#currentColor;
+    }
+
+    // Процесс рисования
+    mousemove(evt) {
+      if (this.#isMouseDown) {
+        let currentPosition = this.#getMousePos(evt);
+        this.ctx.lineTo(currentPosition.x, currentPosition.y);
+        this.ctx.stroke();
+      }
+    }
+
+    // Кнопку мыши отпустили
+    mouseup() {
+      this.#isMouseDown = false;
+    }
+  }
+
+  class Weight {
+    weights = []; // массив весов
+
+    // Инициализация весов
+    initWeights() {
+      const arr = __getArray(__pin(InitWeight(canva.canvasSize)));
+      this.weights = arr;
+
+      console.log('Инициализированные веса:');
+      console.log(this.weights);
+
+      initBtn.disabled = true;
+
+      __unpin(arr);
+    }
+
+    // Загрузка весов из файла
+    load(e) {
+      const file = e.target.files[0];
+
+      let reader = new FileReader();
+
+      reader.readAsText(file);
+
+      reader.onload = function () {
+        let res = reader.result;
+
+        res = res.split(',');
+        res = res.map((e) => Number(e));
+
+        this.weights = res;
+
+        console.log('Загруженные веса:');
+        console.log(res);
+      };
+
+      reader.onerror = function () {
+        console.log(reader.error);
+      };
+
+      initBtn.disabled = true;
+    }
+
+    // Сохранение весов в файл
+    save() {
+      const a = document.createElement('a');
+
+      const file = new Blob([this.weights], { type: 'text/plain' });
+
+      a.href = URL.createObjectURL(file);
+      a.download = 'weights.txt';
+      a.click();
+
+      URL.revokeObjectURL(a.href);
+    }
+  }
+
+  const canva = new Canvas();
+  const weight = new Weight();
 
   // Кнопки
   const correct_cross = document.getElementById('correct_cross');
   const correct_other = document.getElementById('correct_other');
   const predictBtn = document.getElementById('predict');
   const initBtn = document.getElementById('init');
-  const weightMap = document.getElementById('map');
-
-  // Создать поле
-  createCanvas();
 
   // Рисование
-  canvas.addEventListener('mousedown', (event) => mousedown(canvas, event));
-  canvas.addEventListener('mousemove', (event) => mousemove(canvas, event));
-  canvas.addEventListener('mouseup', () => mouseup());
+  canva.canvas.addEventListener('mousedown', (event) => canva.mousedown(event));
+  canva.canvas.addEventListener('mousemove', (event) => canva.mousemove(event));
+  canva.canvas.addEventListener('mouseup', () => canva.mouseup());
 
   // Ивенты
-  document.getElementById('clear').addEventListener('click', createCanvas);
+  document
+    .getElementById('clear')
+    .addEventListener('click', () => canva.clear());
 
-  document.getElementById('load').addEventListener('change', (e) => load(e));
+  document
+    .getElementById('load')
+    .addEventListener('change', (e) => weight.load(e));
+
+  document
+    .getElementById('save')
+    .addEventListener('click', () => weight.save());
+
+  initBtn.addEventListener('click', () => weight.initWeights());
 
   document
     .getElementById('dataset')
     .addEventListener('change', (e) => loadDataset(e));
 
-  document
-    .getElementById('save')
-    .addEventListener('click', () => save(weights));
-
-  correct_cross.addEventListener('click', () => reTrain(true));
-  correct_other.addEventListener('click', () => reTrain(false));
-  predictBtn.addEventListener('click', () => PredictFunc(false));
-  initBtn.addEventListener('click', initWeight);
-  weightMap.addEventListener('click', buildMap);
-
-  // Вычисление сигмоиды
-  function sigmoid(sum) {
-    return 1 / (1 + Math.exp(-sum));
-  }
-
-  // Производная сигмоиды
-  function sigmoid_derivative(sigmoid) {
-    return sigmoid * (1 - sigmoid);
-  }
-
-  // Угадать
-  function PredictFunc(auto) {
-    const data = ctx.getImageData(0, 0, canvasSize, canvasSize);
-    const arr = Array.from(data.data);
-
-    const pixelArr = __pin(__newArray(Int32Array_ID, arr));
-
-    const vectors = __getArray(__pin(toImage(pixelArr, canvasSize)));
-    pixels = vectors;
-
-    const vectorsArr = __pin(__newArray(Int32Array_ID2, pixels));
-    const weightsArr = __pin(__newArray(Float64Array_ID, weights));
-
-    const res = Predict(weightsArr, vectorsArr);
-
-    console.log('Сумма нейрона:');
-    console.log(res);
-    neuronSum = Number(res);
-
-    sigmoidRes = sigmoid(neuronSum);
-
-    !auto && alert(`Это крест на ${(sigmoidRes * 100).toFixed(2)}%`);
-
-    __unpin(pixelArr);
-    __unpin(vectors);
-    __unpin(vectorsArr);
-    __unpin(weightsArr);
-  }
-
-  // Скоректировать веса
-  function reTrain(cross) {
-    const weightsArr = __pin(__newArray(Float64Array_ID, weights));
-    const vectorsArr = __pin(__newArray(Int32Array_ID, pixels));
-
-    let sigmoidDeriv = sigmoid_derivative(sigmoidRes);
-
-    const newWeights = __getArray(
-      __pin(
-        Correct(weightsArr, vectorsArr, neuronSum, 0.05, cross, sigmoidDeriv)
-      )
-    );
-    weights = newWeights;
-
-    console.log('Переобучили');
-
-    __unpin(weightsArr);
-    __unpin(vectorsArr);
-    __unpin(newWeights);
-  }
-
-  // Инициализация весов
-  function initWeight() {
-    const arr = __getArray(__pin(InitWeight(canvasSize)));
-
-    console.log('Инициализированные веса:');
-    console.log(arr);
-    weights = arr;
-
-    initBtn.disabled = true;
-
-    __unpin(arr);
-  }
-
-  // Создание поля
-  function createCanvas() {
-    canvas.id = 'canvas';
-    canvas.width = parseInt(canvasSize);
-    canvas.height = parseInt(canvasSize);
-    canvas.style.zIndex = 8;
-    canvas.style.border = '1px solid';
-    ctx.fillStyle = currentBg;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    let main = document.getElementById('main');
-    main.appendChild(canvas);
-    buttonControl(false);
-  }
-
-  // Автообучение по датасету
-  function autoTrain(fileName) {
-    PredictFunc(true);
-
-    if (fileName.includes('cross') && sigmoidRes < 0.95) reTrain(true);
-    else if (fileName.includes('circle') && sigmoidRes > 0.1) reTrain(false);
-  }
-
-  // Перемешать массив
-  function shuffle(arr = []) {
-    let newArr = [];
-
-    for (let i = arr.length - 1; i >= 0; i--) {
-      let id = Math.floor(Math.random() * arr.length);
-      newArr.push(arr[id]);
-      arr.splice(id, 1);
-    }
-
-    return newArr;
-  }
-
-  // Загрузить датасет
-  function loadDataset(e) {
-    let files = e.target.files;
-    files = Object.values(files);
-
-    files = shuffle(files);
-
-    files.forEach((file) => {
-      const reader = new FileReader();
-      let fileName = file.name;
-
-      reader.onload = (e) => {
-        let img = new Image();
-        img.onload = () => {
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx.drawImage(img, 0, 0);
-
-          autoTrain(fileName);
-        };
-        img.src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    });
-
-    buttonControl(true);
-  }
-
-  // Загрузка весов
-  function load(e) {
-    const file = e.target.files[0];
-
-    let reader = new FileReader();
-
-    reader.readAsText(file);
-
-    reader.onload = function () {
-      let res = reader.result;
-
-      res = res.split(',');
-      res = res.map((e) => Number(e));
-
-      weights = res;
-
-      console.log('Загруженные веса:');
-      console.log(res);
-    };
-
-    reader.onerror = function () {
-      console.log(reader.error);
-    };
-  }
-
-  // Сохранение весов
-  function save(content) {
-    const a = document.createElement('a');
-
-    const file = new Blob([content], { type: 'text/plain' });
-
-    a.href = URL.createObjectURL(file);
-    a.download = 'weights.txt';
-    a.click();
-
-    URL.revokeObjectURL(a.href);
-  }
-
-  function buildMap() {
-    let win = window.open('map.html', '_blank');
-
-    let element = win.document.createElement('script');
-    element.type = 'text/javascript';
-    element.innerHTML = `
-    let w = []
-    w.push(${weights})
-    console.log(w)
-    let table = document.createElement('table');
-    table.border = 1
-
-    for (let i = 0, weigthSize = 0; i < ${canvasSize}; i++){
-    let tr = document.createElement('tr');   
-
-    for (let j = 0; j < ${canvasSize}; j++){
-      let td = document.createElement('td');
-      if (w[weigthSize] > 0 && w[weigthSize] < 0.25) td.style.backgroundColor = 'rgba(0,255,0,0.1)'
-      else if (w[weigthSize] > 0.25) td.style.backgroundColor = 'rgba(0,255,0,0.6)'
-
-      if (w[weigthSize] < 0 && w[weigthSize] > -0.25) td.style.backgroundColor = 'rgba(255,0,0,0.1)'
-      else if (w[weigthSize] < -0.25) td.style.backgroundColor = 'rgba(0,255,0,0.6)'
-
-
-      let text = document.createTextNode(w[weigthSize].toFixed(3));
-
-      td.appendChild(text);
-      tr.appendChild(td);
-      weigthSize++
-    table.appendChild(tr);
-  }
-}
-document.body.appendChild(table);    
-`;
-    setTimeout(function () {
-      win.document.body.appendChild(element);
-    }, 1000);
-  }
-
-  // Позиция мыши
-  function getMousePos(canvas, evt) {
-    let rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top,
-    };
-  }
-
-  // Кнопка мыши зажата
-  function mousedown(canvas, evt) {
-    isMouseDown = true;
-    let currentPosition = getMousePos(canvas, evt);
-    ctx.moveTo(currentPosition.x, currentPosition.y);
-    ctx.beginPath();
-    ctx.lineWidth = currentSize;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = currentColor;
-  }
-
-  // Процесс рисования
-  function mousemove(canvas, evt) {
-    if (isMouseDown) {
-      let currentPosition = getMousePos(canvas, evt);
-      ctx.lineTo(currentPosition.x, currentPosition.y);
-      ctx.stroke();
-    }
-  }
-
-  // Кнопку мыши отпустили
-  function mouseup() {
-    isMouseDown = false;
-    buttonControl(true);
-  }
-
-  // Включение и отключение кнопок
-  function buttonControl(enable) {
-    if (enable) {
-      correct_cross.disabled = false;
-      correct_other.disabled = false;
-      predictBtn.disabled = false;
-    } else {
-      correct_cross.disabled = true;
-      correct_other.disabled = true;
-      predictBtn.disabled = true;
-    }
-  }
+  //   correct_cross.addEventListener('click', () => reTrain(true));
+  //   correct_other.addEventListener('click', () => reTrain(false));
+  //   predictBtn.addEventListener('click', () => PredictFunc(false));
 });
